57c7599f61763e32894af07fca492eab89bea9dd
Gabriel Gomes Coelho <gabrielcoelho2002@gmail.com>
2026-03-12 19:04:34 -0300

    fix: Vite base path from `/family-tree-app-2/` to `/family-tree-app-v2/`.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/family-tree-app-2/',
+  base: '/family-tree-app-v2/',
 })
